16117f2c64e2a9af68db1fdea1f9b34ceac76f13
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2025-08-02 20:05:25 +0700

    add: page & chunk header

--- blog_memory.h
+++ blog_memory.h
@@ -1,6 +1,22 @@
 #ifndef     BLOG_MEMORY_H
 #define     BLOG_MEMORY_H
 
+#include <stddef.h>
+
+
+typedef struct blog_memory_chunk {
+    size_t  capacity;
+    char    flags;
+
+    struct blog_memory_chunk* child;
+} BlogMemoryChunk;
+
+typedef struct blog_memory_page {
+    struct blog_memory_page* next;
+    BlogMemoryChunk*         child;
+} BlogMemoryPage;
+
+
 #ifdef BLOG_MEMORY_IMPLEMENTATION
 // our implementation detail
 #endif
